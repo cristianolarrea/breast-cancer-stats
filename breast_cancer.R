00fed8432470f9d2fb7d7fcb67178d79002e418d
cristiano.leal_ntconsult <junior.larrea.ids@gmail.com>
2023-06-13 19:34:00 -0300

    beggining analysis

--- breast_cancer.R
+++ breast_cancer.R
@@ -27,5 +27,27 @@ df <- df %>% select("diagnosis",
                     "symmetry_mean",
                     "fractal_dimension_mean")
 
-plot(df)
+#Transform target variable into 0 or 1
+df$diagnosis[df$diagnosis=="M"] <- 1
+df$diagnosis[df$diagnosis=="B"] <- 0
+df$diagnosis <- as.numeric(df$diagnosis)
+head(df)
+
+##Seleção do modelo
+#Queremos prever uma variável categórica, logo, teremos que ter um modelo de regressão logística.
+
+#Modelo 1 (mais simples -> gelmann & hill)
+fit.1 <- glm(diagnosis ~ radius_mean, family=binomial(link="logit"), data=df)
+summary(fit.1)
+
+plot(fit.1$residuals) #Não parece haver tendencia
+#Idea: usar ggplot e plotar a smooth, com a curva de incerteza
+
+##Seleção de variáveis
+
+##Avaliação do modelo
+
+#Plot dos resíduos
+
+#AUC
 
